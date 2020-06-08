1e50a7b3bad46a304f8d7773577f88fe36123a01
nalezhytyi <Jediknight05>
2020-06-08 22:48:20 +0300

    Updates

---
+++ precache-manifest.015fc037d95c0c19b3d7f0e6d7c4c951.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46d8469ecee779834d93897931633d6c",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "550ff175b93626b59564",
+    "url": "/monsters/static/css/main.ba8fd78e.chunk.css"
+  },
+  {
+    "revision": "d878ec5bd5e526e0dd46",
+    "url": "/monsters/static/js/2.0ca6e9c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.0ca6e9c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "550ff175b93626b59564",
+    "url": "/monsters/static/js/main.ddb3b93d.chunk.js"
+  },
+  {
+    "revision": "0301f3982c27521ea82e",
+    "url": "/monsters/static/js/runtime-main.b6e496db.js"
+  }
+]);
